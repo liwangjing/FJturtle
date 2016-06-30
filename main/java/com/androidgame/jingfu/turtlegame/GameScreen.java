95174c7da271590f3ba2cfa458f4f5438e80ec20
fengxueem <fengxueem@gmail.com>
2016-06-30 02:06:01 -0500

    Change the size of snake.png and fruit.png, and create MainMenu and Game screen

---
+++ main/java/com/androidgame/jingfu/turtlegame/GameScreen.java
@@ -0,0 +1,44 @@
+package com.androidgame.jingfu.turtlegame;
+
+import com.androidgame.jingfu.fjturtle.framework.Game;
+import com.androidgame.jingfu.fjturtle.framework.Screen;
+
+/**
+ * Created by handsomemark on 6/29/16.
+ */
+public class GameScreen extends Screen {
+
+    public GameScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void paint(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void backButton() {
+
+    }
+}
